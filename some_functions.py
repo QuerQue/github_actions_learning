58b3e246a9b10f3f75a97b19e04f590d373d7c34
Tomek <ntomeky@gmail.com>
2021-12-16 21:18:43 +0100

    First function and tests

---
+++ some_functions.py
@@ -0,0 +1,2 @@
+def hello_name_f(name):
+    return f"Hello {name}"
---
+++ some_unit_tests.py
@@ -0,0 +1,19 @@
+import unittest
+import some_functions
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1,2,3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1,2,3)), 6, "Should be 6")
+
+class TestMyFunctions(unittest.TestCase):
+    
+    def test_hello_name_f(self):
+        self.assertEqual(some_functions.hello_name_f('Tomek'), 'Hello Tomek', "Should be Hello Tomek")
+
+
+if __name__ == '__main__':
+    unittest.main()
